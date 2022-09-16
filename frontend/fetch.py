fc9c36d8bb92dcd10ae1750693057fe095104d58
vykuang <vykuang92@gmail.com>
2022-09-16 23:48:50 +0700

    working pytest monkeypatch demo

--- frontend/fetch.py
+++ frontend/fetch.py
@@ -15,7 +15,7 @@ def get_career_stats(player_id: str, get_request: bool = True) -> dict:
         per_mode36="Per36",
         get_request=get_request,
     )
-    return career_stats
+    return career_stats.get_normalized_json()
 
 
 def get_jsons(career_stats):
