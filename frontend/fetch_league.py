ecb394a0888be61bfc644c6025becc8b2ed9cf07
vykuang <vykuang92@gmail.com>
2022-10-03 23:38:23 +0800

    leaguedash wip

--- frontend/fetch_league.py
+++ frontend/fetch_league.py
@@ -13,8 +13,24 @@ from nba_api.stats.endpoints import leaguedashplayerstats
 from nba_api.stats.static import players
 from tqdm import tqdm
 
-# def get_leaguedash():
-#     """Calls NBA API for league dash player stats endpoint"""
-#     league_dash = leaguedashplayerstats.LeagueDashPlayerStats(
 
-#     )
+def get_leaguedash(
+    measure_type: str = "Base",
+    season_type: str = "Regular Season",
+    season: str = "2001-02",
+    df: bool = True,
+):
+    """Calls NBA API for league dash player stats endpoint"""
+    league_dash = leaguedashplayerstats.LeagueDashPlayerStats(
+        measure_type_detailed_defense=measure_type,
+        season_type_all_star=season_type,
+        season=season,
+        plus_minus="Y",
+        per_mode_detailed="Per100Possessions",
+    )
+    if df:
+        res = league_dash.get_data_frames()[0]
+    else:
+        res = json.loads(league_dash.get_normalized_json())
+
+    return res
