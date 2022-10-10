b88c23e5ee00ebc3b083c070f50ce22e4f4152fb
vykuang <vykuang92@gmail.com>
2022-10-10 15:35:09 +0800

    streamlit wip

--- docs/streamlit.md
+++ docs/streamlit.md
@@ -10,7 +10,7 @@ It's got
 - sliders to filter those choarts
 - maps if we give it lat/long
 
-Pretty impressive stuff.
+Pretty impressive stuff
 
 ## nba_stats
 
@@ -18,14 +18,35 @@ What do I want to show? Ultimately I'd like an interactive applet that lets the 
 
 1. Raw data, after compilation?
 1. Maybe a histogram for the three main stats - PTS, AST, REB, just to visualize the raw data a little
+1. Compare the distributions of stats between the two seasons
 1. Player selection
 1. Predicted player label
+1. Closest players from the comparison season
 1. Visualization of how that player fits within that label?
+   - Bar graph of each stat with that player and the chosen "closest" players from comparison season
    - How would this work?
      - AST vs REB, size=PTS scatterplot of all players with the label, with the selected player highlighted
+   - Array of violinplots showing distribution of stats, indicating where that player was in relation to the current season vs comparison season
+     - use slider for season
+     - use toggles to select which stat to show?
 
 ## Execution
 
 ## Hurdles
+
+Some challenges faced over the course of this project
+
+### Data source
+
+`cumeplayerstat` was my first choice for gathering player stats across an entire season, but couldn't figure out how to pass multiple game IDs, so defaulted back to a more basic endpoint in `playercareerstats`.
+
+Next hurdle was the data extraction speed. Due to some unknown limit placed on the free public API, I had to reduce rate of request. Fetching the stats for every player for any given season would take tens of minutes, and doing so again for other seasons proved impractical if I wanted the app to respond in real time.
+
+Not satisfied with the workflow, I dug around the endpoints and found `leaguedash`, which provides the stats across the whole season, for all players, with two additional perks:
+
+1. One call returns every player
+1. One call for the whole season
+
+The ease of acquiring data from other seasons makes it feasible to start comparing players across eras
 
 ## Docker
--- frontend/app/streamlit_nba.py
+++ frontend/app/streamlit_nba.py
@@ -1,47 +1,66 @@
+"""
+Streamlit demo for NBA stats
+Find similar players across different seasons
+"""
 from pathlib import Path
 
 import altair as alt
 import numpy as np
 import pandas as pd
-
 import streamlit as st
 
 st.title("NBA Player Comparisons across Eras")
 
 
 @st.cache
-def load_career_stats(data_path: Path = "../../data/nba_stats.pkl"):
-    data = pd.read_pickle(data_path)
+def load_career_stats(season: str, data_path: Path = "../../data/"):
+    file_path = data_path / f"leaguedash_merge_{season}.pkl"
+    data = pd.read_pickle(file_path)
     return data
 
 
 data_load_state = st.text("Loading pickle...")
 
-nba_stats = load_career_stats()
+current = load_career_stats(season="2018-19")
+comparison = load_career_stats(season="2004-05")
 
 data_load_state = st.text("Finished loading")
 
-if st.checkbox("Show raw stats"):
-    st.subheader("Raw data")
-    st.dataframe(nba_stats)
+if st.checkbox("Show current season stats"):
+    st.subheader("Current data")
+    st.dataframe(current)
+
+if st.checkbox("Show comparison season stats"):
+    st.subheader("Comparison data")
+    st.dataframe(comparison)
+
 
 st.subheader("EDA")
 
-st.subheader("PTS")
-# reb_filter = st.slider("REBs", 0, 15, 5)
-# reb_filtered = nba_stats[np.around(nba_stats["OREB"] + nba_stats["DREB"]) == reb_filter]
-# reb_filtered_pts = reb_filtered.loc[:, "PTS"]
-# hist_points = np.histogram(reb_filtered_pts, bins=10, range=(0, 35))[0]
+
+st.subheader("AST AND REB VS PTS DISTRIBUTION")
 
 
-hist_points = np.histogram(nba_stats["PTS"], bins=range(0, 35))[0]
+def make_ast_reb_scatter(stats):
+    """Wrapper to make the EDA scatter plot for different seasons"""
+    alt_chart = (
+        alt.Chart(stats)
+        .mark_circle()
+        .encode(x="AST", y="REB", size="PTS", color="FG_PCT", tooltip=["FG_PCT", "TOV"])
+    )
+    return alt_chart
 
-st.bar_chart(hist_points)
 
-st.subheader("PTS, AST, REB")
-alt_chart = (
-    alt.Chart(nba_stats)
-    .mark_circle()
-    .encode(x="AST", y="REB", size="PTS", color="FG_PCT", tooltip=["FG_PCT", "TOV"])
-)
-st.altair_chart(alt_chart)
+current_scatter = make_ast_reb_scatter(current)
+comp_scatter = make_ast_reb_scatter(comparison)
+st.altair_chart(current_scatter)
+st.altair_chart(comp_scatter)
+
+st.subheader("STAT DISTRIBUTION")
+# compares the distribution of each stat between the two seasons
+merge_stats = [stat for stat in current.columns if "_merge" in stat]
+
+
+def make_violin(stats):
+    """Wrapper to return altair violinplot via chart().transform_density()"""
+    np.sum(stats)
